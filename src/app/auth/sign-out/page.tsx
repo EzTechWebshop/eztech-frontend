c57bdc07a044d1b62c831850ee8e329a5e2ed2d2
Rashaun Godding <90447626+rgodding@users.noreply.github.com>
2024-01-13 18:15:39 +0100

    fixed window not defined in sign out

--- src/app/auth/sign-out/page.tsx
+++ src/app/auth/sign-out/page.tsx
@@ -4,6 +4,7 @@ import { useRouter } from "next/navigation";
 
 export default function SignOutPage() {
   const router = useRouter();
+  if (typeof window === "undefined") return null;
   const signout = async () => {
     await signOut({ redirect: false });
     router.push("/");
