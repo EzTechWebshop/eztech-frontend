038daefc9c99136752b0717c5afd66ec7d41266e
Rashaun Godding <90447626+rgodding@users.noreply.github.com>
2024-01-14 13:52:00 +0100

    added alert and confirmation source

---
+++ src/utils/alerts.ts
@@ -0,0 +1,8 @@
+// To be improved with increased user experience
+export function AlertWindow(message: string){
+    return window.alert(message);
+}
+
+export function ConfirmationWindow(message: string){
+    return window.confirm(message);
+}
